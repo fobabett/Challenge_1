a92c1c089a5e360ecd370582ae0c88b960cea97e
Te Vallee <astronomemusic@gmail.com>
2014-10-04 10:17:57 -1000

    Challenge_1 completed

--- challenge_1.js
+++ challenge_1.js
@@ -1,6 +1,15 @@
 exports.sumOfAMultiple = function(n){
-  var sum = 0;
+  var sum = 0; //store the answer
   // do your work here
+  //loop from 1 to 10
+  for (var i =0; i<n; i++){
+    //gets multiples of 3 and 5
+    if(i%3==0 || i%5==0){
+      sum+=i;
+    }
+    //get sum of multiples
   
-  return sum;
+  }
+  
+  return sum; 
 };
